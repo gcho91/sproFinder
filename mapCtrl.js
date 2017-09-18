0d206e470dee27468b5efd71923cd2daa9c44c20
gcho91 <gcho91@gmail.com>
2017-09-18 01:49:08 -0500

    Fixed modal issues

--- mapCtrl.js
+++
@@ -1,5 +0,0 @@
-// angular.module('sproFinder').controller("mapCtrl", function($scope, mapservice){
-//   mapservice.getSomething(75201, "coffee").then(function(response){
-//     console.log(response);
-//   });
-// })
